d8b0ca35ceb6aea8a923e108d838ad1c8ff15855
trmaa <trmaayt@gmail.com>
2024-02-16 19:32:08 +0100

    recipies

---
+++ dist/public/src/redirect.js
@@ -0,0 +1,11 @@
+window.parent.goHome = () => {
+    window.parent.location.href = "https://trmaa.github.io/dist/public/index.html";
+}
+
+window.goHome = () => {
+    window.location.href = "https://trmaa.github.io/dist/public/index.html";
+}
+
+window.goTo = url => {
+	window.location.href = url;
+}
