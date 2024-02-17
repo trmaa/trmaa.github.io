271db2085976b7458e79f581a0e2fc4f8ebc5fdb
trmaa <trmaayt@gmail.com>
2024-02-17 11:25:37 +0100

    search

--- dist/public/src/htmlcount.sh
+++ dist/public/src/htmlcount.sh
@@ -8,11 +8,11 @@ file_array=()
 for file in $html_files; do
     filename=$(basename "$file")
     filename_without_extension="${filename%.*}"
-    file_array+=("\"$filename_without_extension\"")
+    file_array+=("{\"title\":\" $filename_without_extension\"}")
 done
 
 json_file="./dist/public/src/html_files.json"
 json_content=$(printf '%s\n' "${file_array[@]}" | awk '{printf "%s%s",sep,$0; sep=","}') 
-echo "{ \"titles\": [${json_content}] }" > "$json_file"
+echo "[ ${json_content} ]" > "$json_file"
 
 echo "Se ha creado el archivo JSON \"$json_file\" con los nombres de los archivos HTML en \"$directory\"."
--- dist/public/src/recipieslist.js
+++ dist/public/src/recipieslist.js
@@ -1,5 +1,4 @@
 async function read() {
-    const script = await fetch('./src/htmlcount.sh');
     const json = './src/html_files.json';
     try {
         const respuesta = await fetch(json);
@@ -14,5 +13,23 @@ async function read() {
     }
 }
 
-let data = await read();
-console.log(data);
+async function main() {
+    let data = await read();
+    console.log(data);
+
+    let article = document.querySelector("#articulo");
+    article.style.gridTemplateRows = `repeat(${3+Math.floor(data.length/3)},1fr)`;
+
+    let components = data.map(item => `
+        <div class="miniatura">
+           <div class="img"><img src="comunity.jpg"></img></div>
+           <h3>${item.title}</h3>
+           <p>Fantastic dish</p>
+           <div class="button"><button onclick="window.goTo('${item.title}.html')">See more</button></div>
+        </div>
+    `);
+
+    article.innerHTML = components.join('');
+}
+
+main();
