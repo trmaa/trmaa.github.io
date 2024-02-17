27d3a6bc1bcbfba2b2c898c170d361b312c8ad8f
trmaa <trmaayt@gmail.com>
2024-02-17 18:04:26 +0100

    pixabay

--- dist/public/src/htmlcount.sh
+++ dist/public/src/htmlcount.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-directory="./dist/public/"
+directory="./pages/"
 
 html_files=$(find "$directory" -type f -name "*.html")
 
@@ -8,10 +8,12 @@ file_array=()
 for file in $html_files; do
     filename=$(basename "$file")
     filename_without_extension="${filename%.*}"
-    file_array+=("{\"title\":\" $filename_without_extension\"}")
+    # Replace underscores with spaces
+    title=$(echo "$filename_without_extension" | sed 's/_/ /g')
+    file_array+=("{\"title\":\"$title\"}")
 done
 
-json_file="./dist/public/src/html_files.json"
+json_file="./src/html_files.json"
 json_content=$(printf '%s\n' "${file_array[@]}" | awk '{printf "%s%s",sep,$0; sep=","}') 
 echo "[ ${json_content} ]" > "$json_file"
 
--- dist/public/src/recipieslist.js
+++ dist/public/src/recipieslist.js
@@ -1,3 +1,5 @@
+import { pixa,verify } from "./pixabay.js";
+
 async function read() {
     const json = './src/html_files.json';
     try {
@@ -18,16 +20,27 @@ async function main() {
     console.log(data);
 
     let article = document.querySelector("#articulo");
-    article.style.gridTemplateRows = `repeat(${3+Math.floor(data.length/3)},1fr)`;
+    article.style.gridTemplateRows = `repeat(${3 + Math.floor(data.length / 3)}, 1fr)`;
 
-    let components = data.map(item => `
-        <div class="miniatura">
-           <div class="img"><img src="comunity.jpg"></img></div>
-           <h3>${item.title}</h3>
-           <p>Fantastic dish</p>
-           <div class="button"><button onclick="window.goTo('${item.title}.html')">See more</button></div>
-        </div>
-    `);
+    let components = [];
+    for (const item of data) {
+        try {
+            let localImageURL = `./pages/${encodeURIComponent(item.title.trim())}.jpg`;
+            let imageExists = await verify(localImageURL);
+            let imageURL = imageExists ? localImageURL : await pixa(item.title.trim());
+            
+            components.push(`
+                <div class="miniatura">
+                   <div class="img"><img src="${imageURL}"></img></div>
+                   <h3>${item.title}</h3>
+                   <p>Fantastic dish</p>
+                   <div class="button"><button onclick="window.goTo('./pages/${encodeURIComponent(item.title.trim())}.html')">See more</button></div>
+                </div>
+            `);
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
     article.innerHTML = components.join('');
 }
